f3084b11d7d79e317b71b9695dd6a53229ba8e91
DM6377 <dmagrom@gmail.com>
2020-01-07 13:59:32 +0100

    Initial commit

---
+++ app/src/main/java/com/dmagrom/thermosmart/ui/tools/ToolsViewModel.java
@@ -0,0 +1,23 @@
+package com.dmagrom.thermosmart.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel
+        extends ViewModel
+{
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel ()
+    {
+        mText = new MutableLiveData<> ();
+        mText.setValue ("This is tools fragment");
+    }
+
+    public LiveData<String> getText ()
+    {
+        return mText;
+    }
+}
